27fe58071ff9bdb9c50666ce747c3abd55db7175
subby <admin@subby.dev>
2022-07-26 04:43:49 -0700

    moved to KDE

--- backup.sh
+++ backup.sh
@@ -1,52 +1,48 @@
 #!/bin/bash
 
-CONFIG_DIR='~/git/homebase/config'
+CONFIG_DIR='/srv/github/homebase/config'
 
-mkdir -p ~/git/homebase/config/home/.local/share
+mkdir -p /srv/github/homebase/config/home/.local/share
 
 ########## home ##########
 
-cp -r ~/.themes ~/git/homebase/config/home/
-cp -r ~/.icons ~/git/homebase/config/home/
+cp -r ~/.themes /srv/github/homebase/config/home/
+cp -r ~/.icons /srv/github/homebase/config/home/
 
-cp -r ~/.local/share/fonts ~/git/homebase/config/home/.local/share/
-cp -r ~/.vimrc ~/git/homebase/config/home/
-cp -r ~/.xinitrc ~/git/homebase/config/home/
-cp -r ~/.Xresources ~/git/homebase/config/home/
-cp -r ~/.vim ~/git/homebase/config/home/
-cp -r ~/.Xmodmap ~/git/homebase/config/home/
+cp -r ~/.local/share/fonts /srv/github/homebase/config/home/.local/share/
+cp -r ~/.vimrc /srv/github/homebase/config/home/
+cp -r ~/.xinitrc /srv/github/homebase/config/home/
+cp -r ~/.Xresources /srv/github/homebase/config/home/
+cp -r ~/.vim /srv/github/homebase/config/home/
+cp -r ~/.Xmodmap /srv/github/homebase/config/home/
 
 ########## .config ##########
 
-cp -r ~/.config/mpv ~/git/homebase/config/.config/
-cp -r ~/.config/cava ~/git/homebase/config/.config/
-cp -r ~/.config/neofetch ~/git/homebase/config/.config/
-cp -r ~/.config/polybar ~/git/homebase/config/.config/
-cp -r ~/.config/ranger ~/git/homebase/config/.config/
-cp -r ~/.config/nvim ~/git/homebase/config/.config/
-cp -r ~/.config/dunst ~/git/homebase/config/.config/
-cp -r ~/.config/user-dirs.dirs ~/git/homebase/config/.config/
-cp -r ~/.config/htop ~/git/homebase/config/.config/
-cp -r ~/.config/cmus/ ~/git/homebase/config/.config/
-cp -r ~/.config/qutebrowser/ ~/git/homebase/config/.config/
-cp -r ~/.config/zathura/ ~/git/homebase/config/.config/
-cp -r ~/.config/yay/ ~/git/homebase/config/.config/
-cp -r ~/.config/redshift.conf ~/git/homebase/config/.config/
-cp -r ~/.config/mimeapps.list ~/git/homebase/config/.config/
-cp -r ~/.config/compton/ ~/git/homebase/config/.config/
-cp -r ~/.config/bspwm/ ~/git/homebase/config/.config/
-cp -r ~/.config/sxhkd/ ~/git/homebase/config/.config/
-cp -r ~/.config/rofi/ ~/git/homebase/config/.config/
-cp -r ~/.config/oomox/ ~/git/homebase/config/.config/
-cp -r ~/.config/deluge/ ~/git/homebase/config/.config/
-cp -r ~/.config/libinput-gestures.conf ~/git/homebase/config/.config/
+cp -r ~/.config/mpv /srv/github/homebase/config/home/.config/
+cp -r ~/.config/cava /srv/github/homebase/config/home/.config/
+cp -r ~/.config/neofetch /srv/github/homebase/config/home/.config/
+cp -r ~/.config/polybar /srv/github/homebase/config/home/.config/
+cp -r ~/.config/ranger /srv/github/homebase/config/home/.config/
+cp -r ~/.config/nvim /srv/github/homebase/config/home/.config/
+cp -r ~/.config/dunst /srv/github/homebase/config/home/.config/
+cp -r ~/.config/user-dirs.dirs /srv/github/homebase/config/home/.config/
+cp -r ~/.config/htop /srv/github/homebase/config/home/.config/
+cp -r ~/.config/cmus/ /srv/github/homebase/config/home/.config/
+cp -r ~/.config/qutebrowser/ /srv/github/homebase/config/home/.config/
+cp -r ~/.config/zathura/ /srv/github/homebase/config/home/.config/
+cp -r ~/.config/yay/ /srv/github/homebase/config/home/.config/
+cp -r ~/.config/redshift.conf /srv/github/homebase/config/home/.config/
+cp -r ~/.config/mimeapps.list /srv/github/homebase/config/home/.config/
+cp -r ~/.config/compton/ /srv/github/homebase/config/home/.config/
+cp -r ~/.config/bspwm/ /srv/github/homebase/config/home/.config/
+cp -r ~/.config/sxhkd/ /srv/github/homebase/config/home/.config/
+cp -r ~/.config/rofi/ /srv/github/homebase/config/home/.config/
+cp -r ~/.config/oomox/ /srv/github/homebase/config/home/.config/
+cp -r ~/.config/deluge/ /srv/github/homebase/config/home/.config/
+cp -r ~/.config/libinput-gestures.conf /srv/github/homebase/config/home/.config/
 
 ########## /etc ##########
-cp /etc/locale.gen ~/git/homebase/config/etc/
-cp /etc/locale.conf ~/git/homebase/config/etc/
-cp /etc/default/tlp ~/git/homebase/config/etc/default/
-
-sudo cp -r /etc/systemd/logind.conf ~/git/homebase/config/etc/systemd/
-sudo chown -R subby:subby ~/git/homebase/
+cp /etc/locale.gen /srv/github/homebase/config/etc/
+cp /etc/locale.conf /srv/github/homebase/config/etc/
 
 echo Backup Complete
--- install.sh
+++ install.sh
@@ -6,3 +6,109 @@
 #_\ \ |_| | |_) | |_) | |_| |
 #\__/\__,_|_.__/|_.__/ \__, |
 #                      |___/ 
+
+
+# Application Categories ------------------------------------------------------------
+
+NETWORK="archlinux-keyring net-tools wget netctl"
+
+BASE="ttf-ionicons ttf-material-design-icons mpv scrot grsync ranger zip unzip udiskie udisks2 \
+  ntfs-3g chromium git screen tmux \
+  os-prober neovim htop sshfs python-pip zsh htop gparted \
+  p7zip automake"
+
+DRIVERS="piper"
+
+DISPLAY="xorg"
+
+AUDIO="pulseaudio pulseaudio-alsa alsa-utils pulsemixer a52dec x264 x265 \
+  faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis \
+  libxv wavpack x264 xvidcore gstreamer gst-plugins-ugly gst-plugins-base \
+  gst-plugins-bad gst-libav"
+
+EXTRAS=""
+
+# Cleanup Pacman Keys ------------------------------------------------------------
+
+# Setup Locale ------------------------------------------------------------
+
+sudo localedef -f UTF-8 -i en_US en_US.UTF-8
+
+# Configure Time ------------------------------------------------------------
+
+sudo pacman -S ntp --noconfirm
+sudo systemctl enable ntpd.service
+sudo timedatectl set-ntp true
+sudo timedatectl set-timezone America/Los_Angeles
+
+# Setup YAY AUR Package Manager ------------------------------------------------------------
+cd ~/git && git clone https://aur.archlinux.org/yay-git.git && cd yay-git && makepkg -csi && cd ~/git/homebase
+
+# Install Programs ------------------------------------------------------------
+yay -S $NETWORK
+yay -S $BASE
+yay -S $POWER
+yay -S $DISPLAY
+yay -S $AUDIO
+yay -S $EXTRAS
+yay -S $DRIVERS
+
+# System Tweaks ------------------------------------------------------------
+
+echo "fs.inotify.max_user_watches=204800" | sudo tee -a /etc/sysctl.d/90-override.conf
+
+# Setup System Services ------------------------------------------------------------
+
+sudo systemctl enable udisk2.service
+sudo systemctl enable wicd.service
+sudo systemctl enable piper.service
+
+# Configure Programs ------------------------------------------------------------
+
+# Install Rclone
+curl https://rclone.org/install.sh | sudo bash
+
+
+# Setup ST Terminal
+#cd ~/git && git clone https://github.com/callmekory/st &&cd st && sudo make clean install
+
+# Clone Script Git
+#cd ~/ && git clone https://github.com/callmekory/scripts
+
+# Setup nodeJS Global Package Directory
+#mkdir ~/.npm-global && npm config set prefix '~/.npm-global'
+
+# Setup NVIM Plugin Manager
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Setup JAVA
+#sudo archlinux-java set java-10-openjdk
+
+
+
+# Create Needed Directories ------------------------------------------------------------
+#mkdir -p ~/.local/share/Trash/files
+#mkdir ~/.rtorrent.session
+
+# Setup ZSH Shell ------------------------------------------------------------
+git clone --recursive https://github.com/senpaisubby/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+zsh -c 'setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done'
+
+# Change Shell To ZSH
+chsh -s /bin/zsh
+
+echo "INSTALL COMPLETE"
+
+
+
+
+
+
+
+xorg sddm chromium nvidia nvidia-settings discord openssh git plasma plasma-extras kde-applications wget \
+sof-firmware zsh ccsm compiz emerald emerald-themes plexamp-appimage plex-desktop nano linux linux-headers \
+netctl ranger archlinux-keyring
