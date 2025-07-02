b7881f5b360795c5c869e4fb4d762944da6cd87e
Subby <admin@subby.dev>
2025-07-01 21:28:26 -0700

    update dots

---
+++ config/home/.config/hypr/scripts/lock.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+swaylock \
+  --screenshots \
+  --clock \
+  --indicator \
+  --indicator-radius 100 \
+  --indicator-thickness 7 \
+  --effect-blur 7x5 \
+  --effect-vignette 0.5:0.5 \
+  --ring-color 89b4faff \
+  --key-hl-color f38ba8ff \
+  --line-color 00000000 \
+  --inside-color 1e1e2e88 \
+  --separator-color 00000000 \
+  --text-color cdd6f4ff \
+  --text-ver-color cdd6f4ff \
+  --text-wrong-color f38ba8ff \
+  --text-clear-color f9e2afff \
+  --inside-ver-color 89b4fa88 \
+  --inside-wrong-color f38ba888 \
+  --inside-clear-color f9e2af88 \
+  --ring-ver-color 89b4faff \
+  --ring-wrong-color f38ba8ff \
+  --ring-clear-color f9e2afff \
+  --fade-in 0.2 \
+  -f
---
+++ config/home/.config/waybar/scripts/wallpaper.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export PATH="${PATH}:${HOME}/.local/bin/"
+
+DIR=$HOME/Wallpapers
+PICS=($(ls ${DIR}))
+
+RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
+
+if [[ $(pidof swww) ]]; then
+  pkill swww
+fi
+
+swww img ${DIR}/${RANDOMPICS} --transition-type grow --transition-fps 60 --transition-duration 0.5 --transition-bezier 0.65,0,0.35,1 --transition-pos 0.794,0.972 --transition-step 1
+
+sleep 1.25
+
+if [[ "$(<~/.cache/wal/mode )" = "dark" ]]; then
+  wal -i ${DIR}/${RANDOMPICS} --cols16
+elif [[ "$(<~/.cache/wal/mode )" = "light" ]]; then
+  wal -i ${DIR}/${RANDOMPICS} -l --cols16
+fi
+
+pywal-discord -t default
+pywalfox update
+wal-telegram --wal
+
+. $HOME/.config/mako/update-colors.sh
